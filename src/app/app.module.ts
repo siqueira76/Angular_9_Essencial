736d10257137a37de6ab0d069628af9ab9c3ac8e
José Carlos Siqueira <jose.a.siqueira@capgemini.com>
2020-05-25 14:59:43 -0300

    Footer Component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,14 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HeaderComponent } from './component/template/header/header.component';
 
-import { MatToolbarModule } from '@angular/material/toolbar'
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { FooterComponent } from './component/template/footer/footer.component'
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
